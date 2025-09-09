74c198ac16dd5fdcb90ecf418f21e3be35eed920
omorellAUS <32491305+omorellAUS@users.noreply.github.com>
2025-09-09 17:47:39 +0800

    .\build\Impellor.exe

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <string>
 
 int main() {
-    std::cout << "Impellor V1 Build Triggered!" << std::endl;
+    std::string name;
+    std::cout << "Enter your name: ";
+    std::getline(std::cin, name);
+
+    std::cout << "Welcome, " << name << "! Impellor V2 is ready." << std::endl;
     return 0;
 }
+
